44f765205f1e47f46e5f891bfce8aed9457ebbbb
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2022-02-22 22:09:33 -0500

    fixed port

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/imageurl', (req, res) => {
 
 
 
-app.listen(process.env.port || 8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`App is running on ${process.env.PORT}`)
 })
 
